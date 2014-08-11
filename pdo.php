0adf9b8610e4aa377658ef434b17fbe030e35d5f
Albin Larsson <albin.post@gmail.com>
2014-08-11 12:28:34 +0200

    First commit

---
+++ pdo.php
@@ -0,0 +1,62 @@
+<?php
+class Database {
+  /* Database Details */
+  private $host = '';
+  private $user = '';
+  private $pass = '';
+  private $dbname = '';
+
+  private static $_instance = null;
+  private $stmt;
+
+  public function __construct(){
+    try {
+      $this->dbhost = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->dbname, $this->user, $this->pass);
+    } catch(PDOException $e){
+      $this->error = $e->getMessage();
+    }
+  }
+
+  public static function getInstanse() {
+    if(!isset(self::$_instance)) {
+      self::$_instance = new Database();
+    }
+    return self::$_instance;
+  }
+
+  public function query($query) {
+    $this->stmt = $this->dbhost->prepare($query);
+  }
+
+  public function bind($param, $value, $type = null) {
+    if (is_null($type)) {
+      switch (true) {
+        case is_int($value):
+          $type = PDO::PARAM_INT;
+          break;
+        case is_bool($value):
+          $type = PDO::PARAM_BOOL;
+          break;
+        case is_null($value):
+          $type = PDO::PARAM_NULL;
+          break;
+        default:
+        $type = PDO::PARAM_STR;
+      }
+    }
+    $this->stmt->bindValue($param, $value, $type);
+  }
+
+  public function execute() {
+    return $this->stmt->execute();
+  }
+
+  public function resultset() {
+    $this->execute();
+    return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+  public function rowCount() {
+    return $this->stmt->rowCount();
+  }
+}
